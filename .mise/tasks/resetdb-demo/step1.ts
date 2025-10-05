89961e777378367ea9908826e3c5454722cd6116
Jumpei Ogawa <git@phanective.org>
2025-10-05 19:20:07 +0900

    chore: migrate npm script `resetdb-demo` to mise task

---
+++ .mise/tasks/resetdb-demo/step1.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S pnpm exec jiti
+
+import { reset } from "drizzle-seed";
+import { db } from "../../../src/lib/db/db.ts";
+import * as schema from "../../../src/lib/db/schema/index.ts";
+
+await reset(db, schema);
--- scripts/resetdb-demo.ts
+++
@@ -1,5 +0,0 @@
-import { reset } from "drizzle-seed";
-import { db } from "../src/lib/db/db.ts";
-import * as schema from "../src/lib/db/schema/index.ts";
-
-await reset(db, schema);
