821b0f1b796cf5041a87330f3e528944809b9f53
Jumpei Ogawa <git@phanective.org>
2025-10-19 10:01:43 +0900

    fix: remove `<Nav>` from error.tsx
    Instead, add "Try again" button and "Go back to home" button.
    
    You probably cannot call server components from client components.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,17 +1,24 @@
 "use client"; // error.tsx must be a Client Component
 
+import { Button, Link } from "@/components/generic/ButtonLink.tsx";
 import { CenterBox } from "@/components/generic/CenterBox.tsx";
-import { Nav } from "@/components/Nav.tsx";
 
-export default function Error({ error }: {
+export default function Error({ error, reset }: {
   error: Error & { digest?: string; };
+  reset: () => void;
 }) {
   return (
     <>
-      <Nav for="guest" />
       <CenterBox>
         <h1>{ error.message }</h1>
         <p>Sorry, this is probably a bug in this website.</p>
+
+        <Button onClick={() => reset()} theme="button-deep">
+          Try again
+        </Button>
+        <Link href="/" theme="button-deep">
+          Go back to home
+        </Link>
       </CenterBox>
     </>
   );
