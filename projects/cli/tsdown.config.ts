6bc34300d91bb31fecb31af6e6a326ae3c15626a
Jumpei Ogawa <git@phanective.org>
2026-05-29 16:15:14 +0900

    wip: cli

--- projects/cli/tsdown.config.ts
+++ projects/cli/tsdown.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: {
-    cli: "./src/cli.ts",
-    index: "./src/index.ts",
+    "bin/cli": "./src/cli.ts",
+    "dist/index": "./src/index.ts",
   },
+  //outDir: "bin",
   format: [ "esm" ],
+  target: "node24",
 
   dts: true,
   sourcemap: true,
 
   treeshake: false,
   minify: false,
+  exports: true,
   clean: true,
 });
