78fc6bfe87906ac16248728dd97ba4f4058a5ac3
Jumpei Ogawa <git@phanective.org>
2026-06-16 12:54:56 +0900

    chore: upgrade to ESLint v10

--- eslint.config.ts
+++ eslint.config.ts
@@ -27,6 +27,16 @@ const configs = defineConfig([
     },
     rules: {
       "import/extensions": "off",
+
+      // Some rules in eslint-plugin-react are not ready for ESLint v10 yet as of 2026.16.
+      // TODO: enable these rules again when `eslint-plugin-react` is ready for ESLint v10.
+      "react/display-name": "off",
+      "react/no-direct-mutation-state": "off",
+      "react/no-render-return-value": "off",
+      "react/no-string-refs": "off",
+      "react/no-unknown-property": "off",
+      "react/prop-types": "off",
+      "react/require-render-return": "off",
     },
   },
 
