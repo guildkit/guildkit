8b3c1e511c34ca413572e514a254b20b6e68ab8a
Jumpei Ogawa <git@phanective.org>
2026-06-19 15:03:05 +0900

    rerfactor: avoid singleton

--- projects/backend/src/lib/env.ts
+++ projects/backend/src/lib/env.ts
@@ -1,11 +1,14 @@
-import type { Session, User } from "@guildkit/db/auth";
+import type { Auth, Session, User } from "@guildkit/db/auth";
 import type { Env, GuildKitConfig } from "@guildkit/shared";
+import type { PrismaClient } from "./prisma";
 
 export type HonoEnv = {
   Bindings: Env;
   Variables: {
     user?: User;
     session?: Session;
+    auth?: Auth;
+    prisma?: PrismaClient;
     config: GuildKitConfig;
   };
 };
