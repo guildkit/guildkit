7497bbc2ccb3bd103de10af567d00d38ebe75f44
Jumpei Ogawa <git@phanective.org>
2026-06-25 13:08:45 +0900

    chore: set up ESLint for Astro project

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,6 @@
 import { core } from "@phanect/lint";
-import { nextjs } from "@phanect/lint-react";
+import { astro } from "@phanect/lint-astro";
+import { react } from "@phanect/lint-react";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 const configs = defineConfig([
@@ -12,14 +13,16 @@ const configs = defineConfig([
   ]),
 
   ...core,
-  ...nextjs,
+  ...astro,
+  ...react,
 
   {
     // Do not add `files: [ "*" ],` here.
 
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: false,
+        project: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
