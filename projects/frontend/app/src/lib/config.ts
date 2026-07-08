b8258987b32a5fd96ffe0ded2b35b1f3c1b7ad42
Jumpei Ogawa <git@phanective.org>
2026-07-08 16:14:37 +0900

    refactor: allow configuring `backendBaseURL`

--- projects/frontend/app/src/lib/config.ts
+++ projects/frontend/app/src/lib/config.ts
@@ -1,5 +1,7 @@
 import type { GuildKitConfig } from "@guildkit/shared";
 
-declare const __GUILDKIT_PUBLIC_CONFIG__: Pick<GuildKitConfig, "siteName" | "slug">;
+declare const __GUILDKIT_PUBLIC_CONFIG__: Pick<GuildKitConfig, "siteName" | "slug"> & {
+  backendBaseURL: string;
+};
 
-export const { siteName, slug } = __GUILDKIT_PUBLIC_CONFIG__;
+export const { siteName, slug, backendBaseURL } = __GUILDKIT_PUBLIC_CONFIG__;
