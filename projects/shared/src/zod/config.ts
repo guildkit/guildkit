7a40718bc46ad74c75e375a990322beef2821edb
Jumpei Ogawa <git@phanective.org>
2026-07-14 12:11:05 +0900

    feat: add Zod schema for guildkit.config.ts

---
+++ projects/shared/src/zod/config.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import type { S3ClientConfig } from "@aws-sdk/client-s3";
+
+export const GuildKitConfigSchema = z.object({
+  slug: z.string().regex(/^[a-zA-Z0-9_-]+$/),
+  siteName: z.string(),
+  servers: z.object({
+    app: z.enum([ "nodejs", "cloudflare" ]),
+    storage: z.object({
+      bucket: z.string().optional(),
+      cloudflareAccountId: z.string().optional(),
+      client: z.custom<S3ClientConfig>(),
+    }),
+  }),
+  dev: z.object({
+    port: z.number().optional(),
+  }).optional(),
+});
+
+export type GuildKitConfig = z.infer<typeof GuildKitConfigSchema>;
