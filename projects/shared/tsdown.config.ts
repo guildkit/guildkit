e64831fb487cf289e2ad7da36f7f42ab8d8af239
Jumpei Ogawa <git@phanective.org>
2026-07-14 12:42:23 +0900

    chore: set up `@guildkit/shared`
    fixup! chore: set up `@guildkit/shared`

---
+++ projects/shared/tsdown.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: {
+    index: "./src/index.ts",
+  },
+  format: [ "esm" ],
+
+  dts: true,
+  sourcemap: true,
+
+  treeshake: false,
+  minify: false,
+  clean: true,
+
+  exports: {
+    packageJson: false,
+    inlinedDependencies: false,
+  },
+  publint: {
+    level: "suggestion",
+  },
+});
