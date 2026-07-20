998f583780d84695a227c30014f25c3d7c3c3e3e
Jumpei Ogawa <git@phanective.org>
2026-07-20 15:25:34 +0900

    refactor: extract prisma.config.ts to `definePrismaConfig()`

--- projects/backend/exports/prisma.ts
+++ projects/backend/exports/prisma.ts
@@ -1,13 +1,23 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { join } from "node:path";
+import { toMerged } from "es-toolkit";
+import { defineConfig, env, type PrismaConfig } from "prisma/config";
 
-export default defineConfig({
-  schema: "prisma/",
-  migrations: {
-    path: "prisma/migrations",
-    seed: "mise seed",
-  },
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
+export { initPrisma } from "../src/lib/prisma.ts";
+
+export const definePrismaConfig = (config: PrismaConfig = {}): PrismaConfig => defineConfig(
+  toMerged(
+    config,
+    {
+      schema: join(import.meta.dirname, "../prisma/"),
+      migrations: {
+        path: join(process.cwd(), "prisma/migrations"),
+      },
+      datasource: {
+        url: env("DATABASE_URL"),
+      },
+    } satisfies PrismaConfig,
+  )
+);
+
+export type { PrismaClient } from "../src/lib/prisma.ts";
---
+++ projects/backend/prisma.config.ts
@@ -0,0 +1,5 @@
+import { definePrismaConfig } from "./exports/prisma.ts";
+
+const config = definePrismaConfig();
+
+export default config;
